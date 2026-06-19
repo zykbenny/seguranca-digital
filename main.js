e73780cc53fb1e90e352c64ead9c13d91f7ffa77
benny <bianca.graczyk.lima@escola.pr.gov.br>
2026-06-19 13:49:23 +0000

    aula 4

--- main.js
+++ main.js
@@ -5,3 +5,19 @@ numeroSenha.textContent = tamanhoSenha;
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 console.log(botoes)
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+function diminuiTamanho(){
+    if (tamanhoSenha > 1){
+       // tamanhoSenha = tamanhoSenha-1;
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+       // tamanhoSenha = tamanhoSenha+1;
+       tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
