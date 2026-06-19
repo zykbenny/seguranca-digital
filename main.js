97c07b734bc0590d9ebfca5feeefd127e40033d9
benny <bianca.graczyk.lima@escola.pr.gov.br>
2026-06-19 13:41:42 +0000

    aula 3

---
+++ main.js
@@ -0,0 +1,7 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+console.log(botoes)
