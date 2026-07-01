eee9aee05c8a87b71c9aa9cad149680e5159660e
benny <bianca.graczyk.lima@escola.pr.gov.br>
2026-07-01 14:58:07 +0000

    aula 6

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 
-const botoes = document.querySelectorAll('.parametro-senha__botao');
-
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
+
 
 function diminuiTamanho(){
     if (tamanhoSenha > 1){
@@ -15,6 +16,7 @@ function diminuiTamanho(){
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
+
 function aumentaTamanho(){
     if (tamanhoSenha < 20){
        // tamanhoSenha = tamanhoSenha+1;
@@ -23,18 +25,42 @@ function aumentaTamanho(){
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
-const campoSenha = document.querySelector('#campo-senha');
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
-geraSenha();
 
-campoSenha.value = letrasMaiusculas;
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
+const numeros = '0123456789';
+const simbolos = '!@%*?#¨&+-.';
 
 function geraSenha(){
-    let senha = '';
-    for (let i = 0; i < tamanhoSenha;i++){
-        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
-        numeroAleatorio = Math.floor(numeroAleatorio);
-        senha = senha + letrasMaiusculas[numeroAleatorio];
+    let alfabeto = '';
+
+    if (checkbox[0].checked){
+        alfabeto += letrasMaiusculas;
     }
+    if (checkbox[1].checked){
+        alfabeto += letrasMinusculas;
+    }
+    if (checkbox[2].checked){
+        alfabeto += numeros;
+    }
+    if (checkbox[3].checked){
+        alfabeto += simbolos;
+    }
+
+    if (alfabeto.length === 0) {
+        campoSenha.value = '';
+        return;
+    }
+
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha; i++){
+        let numeroAleatorio = Math.floor(Math.random() * alfabeto.length);
+        senha += alfabeto[numeroAleatorio];
+    }
+
     campoSenha.value = senha;
 }
+
+geraSenha();
