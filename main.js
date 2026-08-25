1393aeb523869d1088ac447552b0b7fb20ee1844
benny <bianca.graczyk.lima@escola.pr.gov.br>
2026-08-25 11:09:14 +0000

    arrumando váriaveis- pt dois

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
-const botoes = document.querySelectorAll('.parametro-senha__botao');
-
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
+const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
